9ec4b510ac0368d40bb1a906bbfb10216d13a2e9
snorrehu <snorre.hukkelaas@gmail.com>
2018-12-04 14:48:42 +0100

    Refactoring

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,21 @@
 import Crawlers.PrimePeopleWebCrawler;
 
+
 public class Main {
 
     private static final String html = "http://primepeople.no/ledige-stillinger/";
 
     public static void main(String[] args) {
+        startCrawl();
+
+    }
+
+    public static void startCrawl(){
+        //Prime People
         PrimePeopleWebCrawler primePeopleWebCrawler = new PrimePeopleWebCrawler();
         primePeopleWebCrawler.getPageTableRows(html);
-
         for(Object object: primePeopleWebCrawler.getPositionJSONArray()){
             System.out.println(object);
         }
-
     }
 }
